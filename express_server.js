e7a867c003d9d0a0acef2581315468a9ebeb5a58
Erin Von Gertzen <ianvon@shaw.ca>
2024-02-20 10:42:52 -0700

    updated express server with username passing into EJS template

--- express_server.js
+++ express_server.js
@@ -34,9 +34,9 @@ app.get("/urls/new", (req, res) => {
 // POST Route
 
 app.post('/login', (req, res) => {
-  const username = req.body.username;
-  res.cookie('username', username);
-  res.redirect('/urls');
+  const templateVars = { 
+    username: req.cookies["username"]};
+    res.render("urls_login",templateVars);
 });
 
 
